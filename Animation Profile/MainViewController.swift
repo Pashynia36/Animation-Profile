ececc74c25a0d97ab6de3242affdd225de77eb58
Pavlo Novak <pavlonovak@pavlos-macbook-air.local>
2018-03-14 13:07:38 +0200

    smith

--- Animation Profile/MainViewController.swift
+++ Animation Profile/MainViewController.swift
@@ -10,21 +10,38 @@ import UIKit
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var headerBlur: UIVisualEffectView!
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var tableView: UITableView!
+    var dragged: Int? = nil
     var information = [InfoClass(information: "I love you the more in that I believe you had liked me for my own sake and for nothing else."),
                        InfoClass(information: "But man is not made for defeat. A man can be destroyed but not defeated."),
                        InfoClass(information: "But man is not made for defeat. A man can be destroyed but not defeated."),
                        InfoClass(information: "One thing we DO know is that  whether you've love and won — or loved and lost, it's ALWAYS worth it."),
                        InfoClass(information: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical"),
                        InfoClass(information: "qwerqwetqwetwq"),
-                       InfoClass(information: "wqetwqetwqetwqet"),
-                       InfoClass(information: "asdfg"),
-                       InfoClass(information: "zxcv"),
-                       InfoClass(information: "dfgay"),
-                       InfoClass(information: "eewqay")]
+                       InfoClass(information: "To create an outlet from a constraint, control drag from the constraint in the storyboard editor (just like you would for an outlet of a UIView) into your code."),
+                       InfoClass(information: "You are right - because you are using auto layout and constraints"),
+                       InfoClass(information: "Chillin'"),
+                       InfoClass(information: " I saw some posts about exactly what your saying "),
+                       InfoClass(information: "Any luck yet "),
+                       InfoClass(information: "Christmas!"),
+                       InfoClass(information: "Your question is about the error that is now resolved. You should create a new question to deal with your new issue if needed (after you do lots of research first). But your constraints are going to cause issues trying to change the frame I believe. "),
+                       InfoClass(information: "Is there any height"),
+                       InfoClass(information: "Joe"),
+                       InfoClass(information: "Your Answer is empty."),
+                       InfoClass(information: "Howdy")]
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        profileImage.image = #imageLiteral(resourceName: "profile")
+        backgroundImage.image = #imageLiteral(resourceName: "profile")
+        profileImage.layer.cornerRadius = 50
+        profileName.text = "George"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,7 +54,24 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
-    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if dragged == nil {
+            profileImage.center.x = 50
+            profileImage.center.y = 70
+            profileImage.frame.size.height = profileImage.frame.size.height / 2.0
+            profileImage.frame.size.width = profileImage.frame.size.width / 2.0
+            profileImage.layer.cornerRadius = profileImage.frame.size.height / 2.0
+            
+            profileName.center.y = 40
+            headerView.frame.size.height = headerView.frame.size.height / 2.0
+            headerImage.frame.size.height = headerImage.frame.size.height / 2.0
+            headerBlur.frame.size.height = headerBlur.frame.size.height / 2.0
+            headerImage.image = #imageLiteral(resourceName: "profile")
+            self.view.layoutIfNeeded()
+            
+            dragged = 2
+        }
+    }
     
     
     
