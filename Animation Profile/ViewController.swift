87308386159311a5121bf65872917d25534fa13f
Pavlo Novak <pavlonovak@pavlos-macbook-air.local>
2018-03-14 10:56:15 +0200

    start version

--- Animation Profile/ViewController.swift
+++ Animation Profile/ViewController.swift
@@ -5,21 +5,20 @@
 //  Created by Pavlo Novak on 3/14/18.
 //  Copyright © 2018 Pavlo Novak. All rights reserved.
 //
+// 2 секции
+// 25 ячеек информации о пользователе
+// в верхней ячейке скругленная аватарка и имя под ней
+// фон ячейки = блур фотографии пользователя
+// при прокрутке таблицы верхняя секция занимает 150points и аватарка уходит вверх влево с размером 0.3
 
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
